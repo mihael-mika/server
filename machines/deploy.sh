8e5be4b07b810b7f32a650e4b03da4d5cac9a93c
Žiga Leber <zigaleber7@gmail.com>
2020-03-24 21:23:49 +0100

    Update deploy.sh

--- machines/deploy.sh
+++ machines/deploy.sh
@@ -7,8 +7,8 @@ deploy() {
     NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host "$host"  -I nixos-config="$config"
 }
 
-deploy ssh://bastion './bastion/configure.nix'
-deploy ssh://gateway './gateway/configure.nix'
-deploy ssh://spum_platform './spum-platform/configure.nix'
+deploy bastion './bastion/configure.nix'
+#deploy ssh://gateway './gateway/configure.nix'
+#deploy ssh://spum_platform './spum-platform/configure.nix'
 #deploy ssh://spum_mqtt './spum-mqtt/configure.nix'
 #deploy ssh://spum_docker_registry './spum-docker-registry/configure.nix'

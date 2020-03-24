a9a67d9a4f199d7b68b4f39591d6020a13fba768
Žiga Leber <zigaleber7@gmail.com>
2020-03-24 21:08:51 +0100

    Refine deployment script

--- machines/deploy.sh
+++ machines/deploy.sh
@@ -1,16 +1,14 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p "(nixos {}).nixos-rebuild"
 
-#nixos-rebuild switch --target-host ssh://root@10.17.3.134 -I nixos-config='gateway/configure.nix'
-#nixos-rebuild switch --target-host ssh://root@lpm-bastion.feri.um.si -I nixos-config='bastion/configure.nix'
+deploy() {
+    local host=$1
+    local config=$2
+    NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host "$host"  -I nixos-config="$config"
+}
 
-#NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host ssh://spum_platform -I nixos-config='./spum-platform/configure.nix'
-
-#nixos-rebuild switch --target-host ssh://root@lpm-bastion.feri.um.si -I nixos-config='bastion/configure.nix'
-#NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host ssh://spum_platform -I nixos-config='./spum-platform/configure.nix'
-#NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host ssh://spum_mqtt -I nixos-config='./spum-mqtt/configure.nix'
-#NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host ssh://spum_docker_registry -I nixos-config='./spum-docker-registry/configure.nix'
-
-#NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host ssh://gateway -I nixos-config='./gateway/configure.nix'
-
-NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host ssh://spum_mqtt -I nixos-config='./spum-mqtt/configure.nix'
+deploy ssh://bastion './bastion/configure.nix'
+deploy ssh://gateway './gateway/configure.nix'
+deploy ssh://spum_platform './spum-platform/configure.nix'
+#deploy ssh://spum_mqtt './spum-mqtt/configure.nix'
+#deploy ssh://spum_docker_registry './spum-docker-registry/configure.nix'

7a442aa67aea5deaf7c88a793313d48027c62abd
Žiga Leber <zigaleber7@gmail.com>
2020-03-24 22:56:25 +0100

    Update configure.nix

--- machines/spum-platform/configure.nix
+++ machines/spum-platform/configure.nix
@@ -8,11 +8,8 @@
     ../../users/user.nix
     ../../users/spum.nix
   ];
-  nixpkgs.config.permittedInsecurePackages = [ # XXX: Insecure!
-    "openssl-1.0.2u"
-  ];
-
-  environment.systemPackages = [pkgs.docker-compose pkgs.mongodb];
+  
+  environment.systemPackages = [pkgs.docker-compose pkgs.mongodb-tools];
 
   networking.firewall.allowedTCPPorts = [22 80 5000];
 }

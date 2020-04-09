29f0e08297300618ec7f4035133db5e948ce17a5
marioLSP <62994440+marioLSP@users.noreply.github.com>
2020-04-09 13:48:48 +0200

    Update root.nix

--- users/root.nix
+++ users/root.nix
@@ -1,4 +1,4 @@
 {...}:
 {
-  users.users.root.openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ziga ziga-actions];
+  users.users.root.openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [mario ziga ziga-actions];
 }

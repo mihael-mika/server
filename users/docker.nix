6644507fe6accb5d86376c8485017acc6400abff
Žiga Leber <zigaleber7@gmail.com>
2020-04-13 00:55:31 +0200

    Remove unused user

--- users/docker.nix
+++
@@ -1,9 +0,0 @@
-{config, lib, ...}:
-{
-  users.users.docker = {
-    isNormalUser = true;
-    description = "Docker user";
-    extraGroups = ["docker"];
-    openssh.authorizedKeys.keys = with import ../ssh-keys.nix; [ziga-docker];
-  };
-}

e34d0aa038afea0a1cecfb45968f6579a3bcba63
Žiga Leber <zigaleber7@gmail.com>
2020-04-14 19:27:02 +0200

    Disable deployment

--- machines/deploy.sh
+++ machines/deploy.sh
@@ -7,8 +7,9 @@ deploy() {
     NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host "$host" -I nixos-config="$config" -I nixpkgs='https://github.com/nixos/nixpkgs/archive/29eddfc36d720dcc4822581175217543b387b1e8.tar.gz'
 }
 
-deploy bastion './bastion/configure.nix'
+#deploy bastion './bastion/configure.nix'
 #deploy gateway './gateway/configure.nix'
-deploy spum-platform './spum-platform/configure.nix'
-deploy spum-mqtt './spum-mqtt/configure.nix'
-deploy spum-docker-registry './spum-docker-registry/configure.nix'
+#deploy spum-platform './spum-platform/configure.nix'
+#deploy spum-mqtt './spum-mqtt/configure.nix'
+#deploy spum-docker-registry './spum-docker-registry/configure.nix'
+#deploy builder './builder/configure.nix'

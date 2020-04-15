91c7d6ded8534b625a3c01cd814f7f7ee56fe717
Žiga Leber <zigaleber7@gmail.com>
2020-04-15 22:52:51 +0200

    Update env.sh

--- machines/env.sh
+++ machines/env.sh
@@ -1,3 +1,2 @@
-#export NIX_SSHOPTS="-F $PWD/ssh_config -o UserKnownHostsFile=$PWD/known_hosts/known_hosts" 
-export NIX_SSHOPTS="-F $PWD/ssh_config -o StrictHostKeyChecking=no" 
+export NIX_SSHOPTS="-F $PWD/ssh_config -o UserKnownHostsFile=$PWD/known_hosts/known_hosts" 
 export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz"

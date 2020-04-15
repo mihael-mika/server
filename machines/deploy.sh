eaf29185746229cc9d30da065600c62171e8cc3e
Žiga Leber <zigaleber7@gmail.com>
2020-04-15 13:52:45 +0200

    Reenable deployment

--- machines/deploy.sh
+++ machines/deploy.sh
@@ -7,9 +7,10 @@ deploy() {
     NIX_SSHOPTS='-F ./ssh_config' nixos-rebuild switch --target-host "$host" -I nixos-config="$config" -I nixpkgs='https://github.com/nixos/nixpkgs/archive/708cb6b307b04ad862cc50de792e57e7a4a8bb5a.tar.gz'
 }
 
-#deploy bastion './bastion/configure.nix'
-#deploy gateway './gateway/configure.nix'
+deploy bastion './bastion/configure.nix'
+deploy gateway './gateway/configure.nix'
 #deploy spum-platform './spum-platform/configure.nix'
-#deploy spum-mqtt './spum-mqtt/configure.nix'
+deploy spum-mqtt './spum-mqtt/configure.nix'
 #deploy spum-docker-registry './spum-docker-registry/configure.nix'
-#deploy builder './builder/configure.nix'
+deploy builder './builder/configure.nix'
+deploy grades './grades/configure.nix'

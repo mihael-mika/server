92987889eff00d5b3dd02e44126abf2db9757c76
Žiga Leber <zigaleber7@gmail.com>
2020-09-28 18:31:27 +0200

    Add backups

--- machines/ps/configure.nix
+++ machines/ps/configure.nix
@@ -5,12 +5,20 @@
     ../../modules/image.nix
     ../../modules/base.nix
     ../../modules/docker-host.nix
+    ../../modules/backup.nix
     ../../users/root.nix
     ../../users/user.nix
     ../../users/ps.nix
   ];
 
   environment.systemPackages = [pkgs.docker-compose];
-
   networking.firewall.allowedTCPPorts = [22 5050];
+
+  backup = {
+    interval = "weekly";
+    script = ''
+      mkdir -p /var/lib/backup
+      ${pkgs.python27Packages.rethinkdb}/bin/rethinkdb-dump -c localhost:48015 -f /var/lib/backup/$(date --iso-8601=seconds).tar.gz
+    '';
+  };
 }
---
+++ modules/backup.nix
@@ -0,0 +1,31 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.services.backup; 
+in {
+  options = {
+    options = {
+      enabled = mkEnableOption "Backup machine";
+      interval = mkOption {
+        type = types.str;
+        description = "Backup interval";
+      };
+      script = mkOption {
+        type = types.str;
+        description = "Backup script to run";
+      };
+    };
+  };
+  config = {
+    systemd.timers.backup = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "backup.service" ];
+      timerConfig.OnCalendar = cfg.when;
+    };
+    systemd.services.backup = {
+      serviceConfig.Type = "oneshot";
+      script = cfg.script;
+    };
+  };
+}
+

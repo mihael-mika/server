9ccbf03e790f71d5b044f88cd35cd143ff3e54eb
Žiga Leber <zigaleber7@gmail.com>
2020-09-28 19:58:21 +0200

    Upgrade NixOS version

--- machines/env.sh
+++ machines/env.sh
@@ -1,2 +1,2 @@
 export NIX_SSHOPTS="-F $PWD/ssh_config"
-export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz"
+export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.09.tar.gz"

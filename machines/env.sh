c1c519714b3d50c1c4b68583f4169de12cda4c45
Žiga Leber <zigaleber7@gmail.com>
2020-12-16 00:13:25 +0100

    Update env.sh

--- machines/env.sh
+++ machines/env.sh
@@ -1,2 +1,3 @@
 export NIX_SSHOPTS="-F $PWD/ssh_config"
-export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz"
+
+nixpkgs="nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz"
